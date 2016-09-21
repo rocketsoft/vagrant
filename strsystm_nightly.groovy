8bf038854e4a5d6be6d32cf240d1428de9550072
Ajailal <ajailal.parackal@catalinamarketing.com>
2016-09-21 17:21:04 +0530

    Updating groovy scripts

--- strsystm_nightly.groovy
+++ strsystm_nightly.groovy
@@ -5,79 +5,109 @@ def jsonParse(def json) {
     new groovy.json.JsonSlurperClassic().parseText(json)
 }
 
-stage "Stage : Cleaning (PRE)"
-node('WINHOST') {
-    catchError {
-        print "Removing old vagrant test boxes is exists"
-        bat 'rem vagrant destroy --force'
+stage('Pre Cleaning') {
+    node('VAGRANTHOST') {
+        catchError {
+            print "Removing old vagrant test boxes is exists"
+            bat 'rem vagrant destroy --force'
+        }
     }
 }
 
-stage "Stage : Build"
-node('WINHOST') {
-    print 'Store system build has started'
-    print 'Store system build is finished'
-    
-    print 'POSSimulator build has started'
-    print 'POSSimulator build is finished'
+stage('Build') {
+    node('BUILDMACHINE') {
+        print 'Store system build has started'
+        print 'Store system build is finished'
+        
+        print 'POSSimulator build has started'
+        print 'POSSimulator build is finished'
+    }
 }
 
 try {
-    stage "Stage : Test Machine Setup"
-    node('WINHOST') {
-        print "Checkout Scripts"
-        git branch: 'awards_ust', credentialsId: 'rocketsoft', url: 'https://github.com/rocketsoft/vagrant.git'
-        
-        print "Starting vagrant test machines"
-        bat 'vagrant up'
-        sleep time: 2, unit: 'MINUTES'
-        
-        print "Upgrading store system software in test machines"
-        bat 'vagrant provision'
-        
-        print 'Test machines are up-to-date'
-    }
-    
-    stage "Stage : Unit Test"
-    node('WINHOST') {
-        print "Unit Test has started"
-        print "Unit Test is finished"
-    }
-    
-    stage "Stage : BDD Tests"
-    node('WINHOST') {
-        
-        print "Serving BDD Tests to nodes for parallel execution"
-        def config = jsonParse( readFile('servejobs.json'))
-        
-        def bddTests = [:]
-        
-        for( def i = 0; i < config['tests'].size(); i++) {
+    stage('Test Machine Setup') {
+        node('VAGRANTHOST') {
+            print "Checkout Scripts"
+            git branch: 'awards_ust', credentialsId: 'rocketsoft', url: 'https://github.com/rocketsoft/vagrant.git'
             
-            def test = config['tests'].get(i)
-            bddTests["${test['node']}_${i}"]  = {
+            print "Starting vagrant test machines"
+            bat 'vagrant up'
+            sleep time: 2, unit: 'MINUTES'
+            
+            print "Upgrading store system software in test machines"
+            bat 'vagrant provision'
+            
+            print 'Test machines are up-to-date'
+        }
+    }
+
+    stage('Unit Test') {
+        node('UNITTEST') {
+            print "Unit Test has started"
+            print "Unit Test is finished"
+        }
+    }
+
+    stage('BDD Tests') {
+        node('VAGRANTHOST') {
+            
+            print "Serving BDD Tests to nodes for parallel execution"
+            def config = jsonParse( readFile('bddtests.json'))
+            
+            def bddTests = [:]
+            
+            for( def i = 0; i < config['tests'].size(); i++) {
                 
-                node( test['node']) {
+                def test = config['tests'].get(i)
+                bddTests['bddtest_${i}']  = {
+                    
+                    def testScripts = ""
+                    test['scripts'].each {
+                        testScripts = testScripts + ' ' + it
+                    }
+                    
+                    def testTags = ""
+                    test['tags'].each {
+                        testTags = testTags + 'AND' + it
+                    }
+                    
+                    def testNodes = ""
+                    test['node'].each {
+                        testNodes = testNodes + ' && ' + it
+                    }
+                    
+                    node( testNodes) {
                         print "BDD Test has started"
+                        if ( testScripts != "") {
+                            if ( testTags != "" ) {
+                                print "python -m robot -i ${testTags} ${testScripts} output_${i}.xml"
+                            } else {
+                                print "python -m robot ${testScripts} output_${i}.xml"
+                            }
+                        }
                         print "BDD Test is finished"
+                    }
                 }
             }
+            
+            parallel bddTests
         }
-        
-        parallel bddTests
     }
-    
-    stage "Stage : Reporting"
-    node('WINHOST') {
-        print "Reporting has started"
-        print "Reporting is finished"
+
+    stage('Reporting') {
+        node('VAGRANTHOST') {
+            print "Reporting has started"
+            print "Reporting is finished"
+        }
     }
 } catch (err) {
     echo "Caught: ${err}"
-    
-    stage "Stage : Cleaning (FAILURE)"
-    node('WINHOST') {
-        print "Cleaning (POST) has started"
-        print "Cleaning (POST) is finished"
+    stage(' Post Cleaning') {
+        node('VAGRANTHOST') {
+            print "Cleaning (POST) has started"
+            print "Cleaning (POST) is finished"
+        }
     }
+
+    error 'Build Failed'
 }
